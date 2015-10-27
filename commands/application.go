9824b6d4b6a031fe6244484e5da7a3e3e3eb77d0
diatmpravin <pravinmishra88@gmail.com>
2015-10-27 14:34:48 +0530

    Added rest API endpoint for Get App Instances information

--- commands/application.go
+++ commands/application.go
@@ -392,3 +392,28 @@ func createApp(config *configuration.Configuration, appName string) (app models.
 
 	return
 }
+
+// GetTheInstanceInformation GET instance information of particular app
+func GetTheInstanceInformation(w http.ResponseWriter, r *http.Request) {
+	render := &api.Render{r, w}
+
+	config := configuration.GetDefaultConfig()
+	if err := json.NewDecoder(r.Body).Decode(&config); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	appName := r.URL.Query().Get("appname")
+	repo := CloudControllerApplicationRepository{}
+	app, err := repo.FindByName(config, appName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	instances, err := repo.GetInstances(config, app)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	log.Printf("Detials of a app instances: %+v", instances)
+	render.JSON(instances)
+}
