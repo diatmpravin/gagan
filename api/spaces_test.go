25b6bbd07023d155c41b2367c54b4c884b51136a
diatmpravin <pravinmishra88@gmail.com>
2015-11-04 11:34:07 +0530

    All test pass now

--- api/spaces_test.go
+++ api/spaces_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/diatmpravin/gagan/configuration"
 	"github.com/diatmpravin/gagan/models"
+	"github.com/diatmpravin/gagan/testhelpers"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -84,4 +85,131 @@ func TestFindSpacesWithIncorrectToken(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Equal(t, 0, len(spaces))
+}
+
+var spaceSummaryResponse = testhelpers.TestResponse{Status: http.StatusOK, Body: `
+{
+  "guid": "my-space-guid",
+  "name":"development",
+  "apps":[
+    {
+      "guid":"app-1-guid",
+      "urls":["app1.cfapps.io"],
+      "routes":[
+        {
+          "guid":"route-1-guid",
+          "host":"app1",
+          "domain":{
+            "guid":"domain-1-guid",
+            "name":"cfapps.io"
+          }
+        }
+      ],
+      "running_instances":1,
+      "name":"app1",
+      "memory":128,
+      "instances":1,
+      "state":"STARTED",
+      "service_names":[
+		"my-service-instance"
+      ]
+    },{
+      "guid":"app-2-guid",
+      "urls":["app2.cfapps.io", "foo.cfapps.io"],
+      "routes":[
+        {
+          "guid":"route-2-guid",
+          "host":"app2",
+          "domain":{
+            "guid":"domain-1-guid",
+            "name":"cfapps.io"
+          }
+        }
+      ],
+      "running_instances":1,
+      "name":"app2",
+      "memory":512,
+      "instances":3,
+      "state":"STARTED",
+      "service_names":[
+		"my-service-instance"
+      ]
+    }
+  ],
+  "services": [
+    {
+      "guid": "my-service-instance-guid",
+      "name": "my-service-instance",
+      "bound_app_count": 2,
+      "service_plan": {
+        "guid": "service-plan-guid",
+        "name": "spark",
+        "service": {
+          "guid": "service-offering-guid",
+          "label": "cleardb",
+          "provider": "cleardb-provider",
+          "version": "n/a"
+        }
+      }
+    }
+  ]
+}`}
+
+var spaceSummaryEndpoint = testhelpers.CreateEndpoint(
+	"GET",
+	"/v2/spaces/my-space-guid/summary",
+	nil,
+	spaceSummaryResponse,
+)
+
+func TestGetSummary(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(spaceSummaryEndpoint))
+	defer ts.Close()
+
+	repo := CloudControllerSpaceRepository{}
+	config := &configuration.Configuration{
+		AccessToken: "BEARER my_access_token",
+		Target:      ts.URL,
+		Space:       models.Space{Guid: "my-space-guid"},
+	}
+
+	space, err := repo.GetSummary(config)
+	assert.NoError(t, err)
+	assert.Equal(t, "my-space-guid", space.Guid)
+	assert.Equal(t, "development", space.Name)
+	assert.Equal(t, 2, len(space.Applications))
+	assert.Equal(t, 1, len(space.ServiceInstances))
+
+	app1 := space.Applications[0]
+	assert.Equal(t, app1.Name, "app1")
+	assert.Equal(t, app1.Guid, "app-1-guid")
+	assert.Equal(t, len(app1.Urls), 1)
+	assert.Equal(t, app1.Urls[0], "app1.cfapps.io")
+
+	assert.Equal(t, app1.State, "started")
+	assert.Equal(t, app1.Instances, 1)
+	assert.Equal(t, app1.RunningInstances, 1)
+	assert.Equal(t, app1.Memory, 128)
+
+	app2 := space.Applications[1]
+	assert.Equal(t, app2.Name, "app2")
+	assert.Equal(t, app2.Guid, "app-2-guid")
+	assert.Equal(t, len(app2.Urls), 2)
+	assert.Equal(t, app2.Urls[0], "app2.cfapps.io")
+	assert.Equal(t, app2.Urls[1], "foo.cfapps.io")
+
+	assert.Equal(t, app2.State, "started")
+	assert.Equal(t, app2.Instances, 3)
+	assert.Equal(t, app2.RunningInstances, 1)
+	assert.Equal(t, app2.Memory, 512)
+
+	instance1 := space.ServiceInstances[0]
+	assert.Equal(t, instance1.Name, "my-service-instance")
+	assert.Equal(t, instance1.ServicePlan.Name, "spark")
+	assert.Equal(t, instance1.ServicePlan.ServiceOffering.Label, "cleardb")
+	assert.Equal(t, instance1.ServicePlan.ServiceOffering.Provider, "cleardb-provider")
+	assert.Equal(t, instance1.ServicePlan.ServiceOffering.Version, "n/a")
+	assert.Equal(t, len(instance1.ApplicationNames), 2)
+	assert.Equal(t, instance1.ApplicationNames[0], "app1")
+	assert.Equal(t, instance1.ApplicationNames[1], "app2")
 }
